bcb0eb85971fff77fb57dfb32ae4710376ae9407
end3rs-army <eric.m.dowty@gmail.com>
2015-06-14 16:43:37 -0600

    finished setup for omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   resources :bathrooms, only: [:index]
 
   get    '/auth/:provider/callback', to: 'sessions#create'
-  get    '/login', to: 'sessions#login'
-  delete '/logout', to: 'sessions#destroy'
+  get                      '/login', to: 'sessions#new'
+  delete                  '/logout', to: 'sessions#destroy'
 
-  root 'sessions#login'
+  root 'sessions#new'
 
 end

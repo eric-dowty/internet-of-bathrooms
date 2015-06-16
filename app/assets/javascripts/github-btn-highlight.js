d5faf6553b1e160a28679b40af4a7cc6bca81bab
end3rs-army <eric.m.dowty@gmail.com>
2015-06-15 21:13:06 -0600

    homepage complete

---
+++ app/assets/javascripts/github-btn-highlight.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+  $('#btn-github-img').mouseenter(function(){
+    console.log($(this).parent)
+  });
+
+  $('#btn-github-img').mouseleave(function(){
+  });
+
+});

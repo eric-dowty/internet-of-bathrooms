4ab791a3135ba5b79d4cf2c51c9cf4f9ecdaa5eb
end3rs-army <eric.m.dowty@gmail.com>
2015-06-16 12:02:57 -0600

    added ajax call for symbol transition

--- app/controllers/bathrooms_controller.rb
+++ app/controllers/bathrooms_controller.rb
@@ -1,8 +1,10 @@
 class BathroomsController < ApplicationController
-
+  respond_to :json, :html
   before_action :authorize!, only: [:main]
 
   def main
+    @toilet_status = ToiletService.new.usage_data
+    respond_with @toilet_status
   end
 
   def home

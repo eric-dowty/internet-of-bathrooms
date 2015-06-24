afe21a09350490b3224fdcd360623f889f97e1b4
end3rs-army <eric.m.dowty@gmail.com>
2015-06-24 14:32:48 -0600

    worked out bot bug

--- app/assets/javascripts/bot.js
+++ app/assets/javascripts/bot.js
@@ -5,18 +5,18 @@ function botInfo(){
   Tipped.create('#bathroom-bot-about-btn', "Bathroom-Bot will send a personal message to Slack #watercooler when a bathroom becomes available.");
 
   $('#bathroom-bot-question').click(function(){
-
+    var notice = $('#bathroom-bot-question').html(); 
+    $('#bathroom-bot-question').empty();
+    if(notice === "Click to enable<br>Bathroom-Bot"){
+      $('#bathroom-bot-question').append("Bot enabled!!<br>Click to disable");  
+    } else {
+      $('#bathroom-bot-question').append("Click to enable<br>Bathroom-Bot");  
+    }
     $.ajax({
-      url: "/bot_update",
-      dataType: 'json',
-      success: function(success){
-      },
-      error: function(error){
-        console.log(error)
-      }
+      url: "/bot_update"
     });
-
   })
+
 }
 
 
